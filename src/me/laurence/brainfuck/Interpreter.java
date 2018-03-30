4e38dd1e7705c1f27241213e42c08b8c935b4783
Laurence <hii488@btinternet.com>
2018-03-30 16:45:30 +0100

    Added support for custom brainfuck 'languages'

--- src/me/laurence/brainfuck/Interpreter.java
+++ src/me/laurence/brainfuck/Interpreter.java
@@ -7,12 +7,47 @@ public class Interpreter {
 
 	private Cell cell;
 	private  ArrayList<Integer> loopBeginnings;
+	private String[] language;
 	private Scanner scan;
+	private String unusedString;
+	private int longestLength;
 	public boolean integerValues;
 	
 	public Interpreter() {
+		this(new String[] {">", "<", "+", "-", ".", ",", "[", "]"});
+	}
+	
+	public Interpreter(String[] language) {
+		checkLanguage(language);
+		this.language = language;
 		reset();
 		scan = new Scanner(System.in);
+	}
+	
+	private void checkLanguage(String[] language) {
+		if(language.length != 8) throw new IllegalArgumentException("Language must have length 8.");
+		
+		for(int i = 0; i < 8; i++) {
+			
+			if(language[i].length() == 0) throw new IllegalArgumentException("Language cannot have empty commands.");
+			if(language[i].length() > longestLength) longestLength = language[i].length();
+			
+			for(int j = i+1; j < 8; j++) {
+				if(language[i].equals(language[j])) throw new IllegalArgumentException("Two commands cannot be the same");
+			}
+		}
+		
+		boolean used;
+		int unused = 0; // numbers are used as the language must be 8 strings long, so there must be a digit from 0-9 that is not used as a command.
+		do {
+			used = false;
+			for(int i = 0 ; i < 8; i++)
+				if(language[i].equals(unused + "")) used = true;
+			if(used) unused++;
+		} while(used);
+		unusedString = unused + "";
+		
+		// TODO: Check that a command is not a concatenation of other commands.
 	}
 	
 	public void reset() {
@@ -24,12 +59,16 @@ public class Interpreter {
 	public boolean isValid(String s) {
 		
 		int bracketCount = 0;
+		String open = language[6];
+		String close = language[7];
+		
 		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			switch(c) {
-			case '[': bracketCount++; break;
-			case ']': bracketCount--; break;
-			}
+			String c = getNextCommand(s.substring(i));
+			
+			if(c.equals(open)) bracketCount++;
+			else if(c.equals(close)) bracketCount--;
+			
+			i += c.length()-1;
 		}
 		
 		return bracketCount == 0;
@@ -38,60 +77,70 @@ public class Interpreter {
 	public void interpret(String s) {
 		if(!isValid(s)) return;
 		
+		String c;
 		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			switch(c) {
-			case '>':
-				cell = cell.getNext();
-				break;
-				
-			case '<':
-				cell = cell.getPrev();
-				break;
-				
-			case '+':
-				cell.increment();
-				break;
-				
-			case '-':
-				cell.decrement();
-				break;
-				
-			case '.':
+			c = getNextCommand(s.substring(i));
+			i += c.length()-1;
+			
+			if(c.equals(language[0]))      cell = cell.getNext();
+			else if(c.equals(language[1])) cell = cell.getPrev();
+			else if(c.equals(language[2])) cell.increment();
+			else if(c.equals(language[3])) cell.decrement();
+			
+			else if(c.equals(language[4])) {
 				System.out.println((char) cell.getValue());
 				if(integerValues) System.out.println("::" + cell.getValue());
-				break;
+			}
 			
-			case ',':
+			else if(c.equals(language[5])) {
 				System.out.print("> ");
 				char ch = scan.nextLine().toCharArray()[0];
 				cell.setValue((short) ch);
 				if(integerValues) System.out.println("::" + (int)ch + "\n");
-				break;
-				
-			case '[':
+			}
+			
+			else if(c.equals(language[6])) {
 				if(cell.getValue() != 0)
 					loopBeginnings.add(i);
 				else {
 					int bracketCount = 1;
-					while(bracketCount != 0 || s.charAt(i) != ']') {
-						i++;
-						if(s.charAt(i) == '[') bracketCount++;
-						else if(s.charAt(i) == ']') bracketCount--;
+					i++;
+					while(bracketCount != 0) {
+						c = getNextCommand(s.substring(i));
+						i += c.length();
+						if(c.equals(language[6])) bracketCount++;
+						else if(c.equals(language[7])) bracketCount--;
 					}
+					i--;
 				}
-				break;
-				
-			case ']':
+			}
+			
+			else if(c.equals(language[7])) {
 				if(cell.getValue() != 0) 
 					i = loopBeginnings.get(loopBeginnings.size()-1);
-				
 				else
 					loopBeginnings.remove(loopBeginnings.size()-1);
-				
-				break;
 			}
 		}
+	}
+	
+	private String getNextCommand(String s) {
+		int end = s.length() < longestLength ? s.length() : longestLength;
+		
+		for(int i = 0; i < end; i++)
+			for(int j = 0; j < 8; j++)
+				if(language[j].equals(s.substring(0, end-i))) return s.substring(0, end-i);
+		
+		return unusedString; // has length 1 so un-found commands do not halt the program.
+	}
+
+	public String[] getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String[] language) {
+		checkLanguage(language);
+		this.language = language;
 	}
 	
 }
